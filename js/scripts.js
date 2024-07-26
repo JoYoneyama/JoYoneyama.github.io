e8f139b18536e429bf1e994127004f4d0d3b12f8
JoYoneyama <jvsyoneyama@gmail.com>
2024-07-26 14:14:50 -0300

    Animações

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,24 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+
+//animacao so acontece quando o elemento entra no viewport da tela
+
+document.addEventListener("DOMContentLoaded", () => {         //checa se o documento carregou
+
+    const observer = new IntersectionObserver(entries => {       //usa IntersectionObserver para ver se o elemento esta no viewport
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('in-view');
+          return;
+        }
+        
+      })
+    })
+  
+    const allAnimatedElements = document.querySelectorAll('.animate');       //pega todos os elementos com .animate aplicados
+  
+    allAnimatedElements.forEach((element) => observer.observe(element));      //adiciona o observer para todos esses elementos
+  
+  })
